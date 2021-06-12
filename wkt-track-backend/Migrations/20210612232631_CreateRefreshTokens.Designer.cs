b2a2cffede15b942e6bd438a47233c9c5bd52a12
TheWolfInstinct <fixer3838@live.fr>
2021-06-13 01:27:28 +0200

    ADDED migration files for the RefreshToken table creation

--- wkt-track-backend/Migrations/20210612232631_CreateRefreshTokens.Designer.cs
+++ wkt-track-backend/Migrations/20210612232631_CreateRefreshTokens.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using wkt_track_backend.Data;
@@ -9,9 +10,10 @@ using wkt_track_backend.Data;
 namespace wkt_track_backend.Migrations
 {
     [DbContext(typeof(WktDbContext))]
-    partial class WktDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210612232631_CreateRefreshTokens")]
+    partial class CreateRefreshTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ wkt-track-backend/Migrations/20210612232631_CreateRefreshTokens.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace wkt_track_backend.Migrations
+{
+    public partial class CreateRefreshTokens : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    ValidUntil = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: true),
+                    user_id = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.Token);
+                    table.ForeignKey(
+                        name: "FK_RefreshTokens_AspNetUsers_user_id",
+                        column: x => x.user_id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_user_id",
+                table: "RefreshTokens",
+                column: "user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+        }
+    }
+}
