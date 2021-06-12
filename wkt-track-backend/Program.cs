9b9355e2a6d5410b5c5db83d0b32d36770bc2e3f
TheWolfInstinct <fixer3838@live.fr>
2021-06-13 00:56:24 +0200

    ADDED execution of Seed method from DbSeed class on startup

--- wkt-track-backend/Program.cs
+++ wkt-track-backend/Program.cs
@@ -3,25 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using wkt_track_backend.Data;
 
 namespace wkt_track_backend
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().SeedDatabase().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseUrls("http://*:2000");
                     webBuilder.UseStartup<Startup>(); 
                 });
+
+        private static IHost SeedDatabase(this IHost iHost)
+        {
+            var servicesScopeFactory = (IServiceScopeFactory) iHost
+                .Services.GetService(typeof(IServiceScopeFactory));
+            using (var scope = servicesScopeFactory.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                var dbContext = services.GetRequiredService<WktDbContext>();
+                DbSeed.Seed(userManager, roleManager, dbContext);
+            }
+
+            return iHost;
+        }
     }
 }
