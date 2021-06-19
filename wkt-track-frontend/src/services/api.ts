a65877d9b4048703617a18fead499c7e9c3ba56f
TheWolfInstinct <fixer3838@live.fr>
2021-06-20 00:10:05 +0200

    ADDED comments explaining the base api interceptors

--- wkt-track-frontend/src/services/api.ts
+++ wkt-track-frontend/src/services/api.ts
@@ -6,8 +6,14 @@ const api = axios.create({
   withCredentials: true
 })
 
+// IF response status is 401 on an URL (possibly JWT token expired)
+// Send a request to /refreshtoken
+// If refreshtoken cookie is still valid, request will give back a new jwt token
+// With new jwt token, send back the original request
+// The refresh of the token will be done seamlessly for the end user
 // @ts-ignore
 api.interceptors.response.use(null, (error) => {
+  // If the request hasn't been retried yet and status is 401
   if(
       error.config &&
       error.response?.status === 401 &&
@@ -15,6 +21,7 @@ api.interceptors.response.use(null, (error) => {
   )
   {
     return new Promise((resolve, reject) => {
+      // Send request to /refreshtoken url
       refreshToken(axios, error.config)
           .then((result: any) => {
             resolve(result)
@@ -29,6 +36,7 @@ api.interceptors.response.use(null, (error) => {
 
 const refreshToken = (axios: AxiosInstance, config: AxiosRequestConfig) => {
   return new Promise((resolve, reject) => {
+    // send request to /refreshtoken
     api.post(API_URL + 'refreshtoken', {
       headers: {
         withCredentials: true
@@ -36,8 +44,11 @@ const refreshToken = (axios: AxiosInstance, config: AxiosRequestConfig) => {
     })
         .then((res: AxiosResponse) => {
           console.log(res.data);
+          // Set new jwt_token gained via the refresh token²
           localStorage.setItem('jwt_token', res.data);
+          // set the header with the new jwt token
           config.headers = { 'Authorization': 'Bearer ' + localStorage.getItem('jwt_token') }
+          // send back the original request
           axios.request(config)
               .then((result: AxiosResponse) => {
                 return resolve(result);
@@ -47,6 +58,12 @@ const refreshToken = (axios: AxiosInstance, config: AxiosRequestConfig) => {
                 return reject(err);
               });
         })
+
+        // In case there is an error and there might be no refresh token cookie
+        // Which gives back a 400 status due to dotnet BadRequest directive in the refreshToken endpoint if there is no
+        // cookie set
+        // Need to remove the expired jwt_token aswell
+        // Need to redirect the user to login page (to be done)
         .catch((err: AxiosError) => {
           console.log(err);
           console.log(err.response?.status);
