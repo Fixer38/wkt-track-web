4455f0367103196305c4a911ac344339bf436933
TheWolfInstinct <fixer3838@live.fr>
2021-06-26 01:37:13 +0200

    ADDED form with react-hook-form for signup

--- wkt-track-frontend/src/components/signup.tsx
+++ wkt-track-frontend/src/components/signup.tsx
@@ -1,18 +1,25 @@
-import { useForm } from "react-hook-form";
+import {SubmitHandler, useForm} from "react-hook-form";
 import {useDispatch, useSelector} from "react-redux";
 import { useHistory } from "react-router-dom";
 import {userSelector} from "../features/User/UserSlice";
 import {useEffect} from "react";
+import { Fragment } from "react";
+
+type FormValues = {
+  username: string;
+  email: string;
+  password: string;
+}
 
 const Signup = () => {
   const dispatch = useDispatch();
-  const { register, formState: { errors }, handleSubmit } = useForm();
+  const { register, formState: { errors }, handleSubmit } = useForm<FormValues>();
   const history = useHistory();
 
   const { isFetching, isSuccess, isError, errorMessage } = useSelector(
       userSelector
   );
-  const onSubmit = (data) => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
     dispatch(signupUser(data));
   }
 
@@ -28,6 +35,57 @@ const Signup = () => {
       dispatch(clearState());
     }
   }, [isSuccess, isError]);
+
+  return (
+      <Fragment>
+        <div>
+          <div>
+            <h2>Sign up</h2>
+          </div>
+
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            method="POST"
+          >
+
+            <label
+                htmlFor="username"
+            >
+            </label>
+            <input
+              id="username"
+              type="text"
+              autoComplete="username"
+              required
+              {...register("username", { required: true})}
+            />
+
+            <label
+              htmlFor="email"
+            >
+            </label>
+            <input
+              id="email"
+              type="text"
+              autoComplete="email"
+              required
+              {...register("email", { required: true})}
+            />
+
+            <label
+                htmlFor="password"
+            >
+            </label>
+            <input
+              id="password"
+              type="password"
+              required
+              {...register("password", { required: true})}
+            />
+          </form>
+        </div>
+      </Fragment>
+  )
 }
 
 
