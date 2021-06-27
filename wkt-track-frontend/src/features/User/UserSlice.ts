f91b48eb50876aba4797f76450761c1131bf160f
TheWolfInstinct <fixer3838@live.fr>
2021-06-27 23:48:42 +0200

    ADDED signupUser Thunk and its interface used

--- wkt-track-frontend/src/features/User/UserSlice.ts
+++ wkt-track-frontend/src/features/User/UserSlice.ts
@@ -1,4 +1,7 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import axios from '../../services/api';
+
+const API_URL = "http://localhost:2000/user"
 
 interface RootState {
   user: UserInterface
@@ -18,7 +21,6 @@ export const userSlice = createSlice({
   initialState: {
     username: '',
     email: '',
-    jwt: '',
     isFetching: false,
     isError: false,
     isSuccess: false,
@@ -31,5 +33,60 @@ export const userSlice = createSlice({
 
   },
 })
+
+interface signupUserReturn {
+  data: String,
+  username: String,
+  email: String
+}
+
+interface ISignupUser {
+  username: String,
+  email: String,
+  password: String,
+}
+
+interface signupUserError {
+  errorMessage: String
+}
+
+export const signupUser = createAsyncThunk<
+    signupUserReturn,
+    ISignupUser,
+    {
+      extra: {
+        jwt: string
+      }
+      rejectedValue: signupUserError
+    }
+    >(
+    'users/signupUser',
+    async (form_data: ISignupUser, thunkApi) => {
+      const { username, email, password } = form_data
+      try {
+        const response = await axios.post(
+            API_URL + "register", {
+              username,
+              email,
+              password,
+            }
+        )
+        let data = await response.data;
+        console.log(data);
+
+        if(response.status === 200) {
+          localStorage.setItem('jwt_token', data);
+          return { ...data, username: username, email: email } as signupUserReturn;
+        }
+        else {
+          return thunkApi.rejectWithValue(data as signupUserError);
+        }
+      }
+      catch (e) {
+        console.log("API error: ", e.response.data);
+        return thunkApi.rejectWithValue(e.response.data);
+      }
+    }
+)
 
 export const userSelector = (state: RootState) => state.user;
