4d9ab3d2600d2089e34e9f7a4613592aca2941f2
TheWolfInstinct <fixer3838@live.fr>
2021-06-30 00:41:17 +0200

    added the use of router to reach component

--- wkt-track-frontend/src/App.tsx
+++ wkt-track-frontend/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Signup from "./components/signup";
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact component={Signup} path="/signup" />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
