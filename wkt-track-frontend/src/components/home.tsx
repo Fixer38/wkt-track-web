af306d08ec40e1e42bd9857dbc0a9afacc90dbc8
Fixer <fixer3838@live.fr>
2021-07-07 19:40:33 +0200

    removed @material-ui/core using tailwind css instead

--- wkt-track-frontend/src/components/home.tsx
+++ wkt-track-frontend/src/components/home.tsx
@@ -2,25 +2,11 @@ import {Fragment, useCallback} from "react";
 import { Link } from "react-router-dom";
 import {useSelector} from "react-redux";
 import {userSelector} from "../features/User/UserSlice";
-import {AppBar, Button, createStyles, makeStyles, Theme, Toolbar, Typography} from "@material-ui/core";
 
 // TODO: Separate navbar into navbar component do display it on all pages
-// TODO: Create proper design with Figma
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    title: {
-      flexGrow: 1,
-      textAlign: 'center',
-    },
-  }),
-);
+// TODO: Create proper design
 
 const Home = () => {
-  const classes = useStyles();
   const { isLoggedIn } = useSelector(
     userSelector
   );
@@ -33,24 +19,16 @@ const Home = () => {
     else {
       return (
         <Fragment>
-          <Button color="inherit"><Link to="/signup">Signup</Link></Button>
-          <Button color="inherit"><Link to="/login">Login</Link></Button>
+          <Link to="/signup">Signup</Link>
+          <Link to="/login">Login</Link>
         </Fragment>
       )
     }
   }, [isLoggedIn])
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Wkt-Track
-          </Typography>
-          <Button color="inherit"><Link to="/home">Home</Link></Button>
-          {renderIfLoggedIn()}
-        </Toolbar>
-      </AppBar>
+    <div>
+
     </div>
   )
 }
